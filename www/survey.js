fc69cb110e3c17b35ac372b2e100c83a610b5e6f
bitsway <bitsway@gmail.com>
2016-03-06 13:15:25 +0600

    t 1 20160306

---
+++ www/survey.js
@@ -0,0 +1,27 @@
+
+
+
+function get_login() {
+	var url = "#login";
+	$.mobile.navigate(url);
+	}
+
+							
+//========================= Longin: Check user
+function check_user() {
+	alert ('Nadira')
+	
+	//localStorage.List_data=localStorage.List_data+	'<li style="height:100px"><input class= "qty" type="text" id="item1" style="width:50px" placeholder="Search"><strong class="name">Andrew</strong><small class="description">bb</small> </li>  ' 
+//	localStorage.List_data=	localStorage.List_data+'<li style="height:100px"><input class= "qty" type="text" id="item1" style="width:50px" placeholder="Search"><strong class="name">Andrew</strong><small class="description">bb</small> </li>  ' 
+//	localStorage.List_data=	localStorage.List_data+'<li style="height:100px"><input class= "qty" type="text" id="item1" style="width:50px" placeholder="Search"><strong class="name">Andrew</strong><small class="description">bb</small> </li>  ' 
+//	localStorage.List_data=	localStorage.List_data+'<li style="height:100px"><input class= "qty" type="text" id="item1" style="width:50px" placeholder="Search"><strong class="name">Andrew</strong><small class="description">bb</small> </li>  ' 
+//	localStorage.List_data=localStorage.List_data+	'<li style="height:100px"><input class= "qty" type="text" id="item1" style="width:50px" placeholder="Search"><strong class="name">Andrew</strong><small class="description">bb</small> </li>  ' 
+//
+//	//$("#mylist").append(localStorage.List_data);
+//	
+//	alert (localStorage.List_data)
+	
+	
+	}//function
+
+
